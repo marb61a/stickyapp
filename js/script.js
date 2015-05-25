530430f5ec2a9e1d05bf13f52f0ae286d28de583
marb <marb61a@gmail.com>
2015-05-25 18:04:05 +0000

    adding drag and drop

--- js/script.js
+++ js/script.js
@@ -9,6 +9,10 @@ if(window.openDatabase){
     alert("Failed to open database, make sure your browser supports HTML5 web storage");
 }
 
+var captured = null;
+var highestZ = 0;
+var highestId = 0;
+
 function Note(){
     var self = this;
     
@@ -97,5 +101,89 @@ Note.prototype = {
 	},
 	set zIndex(x){
 	    this.note.style.zIndex = x;
-	}
+	},
+	close: function(e){
+		this.cancelPendingSave();
+		var note = this;
+		db.transaction(function(tx){
+			tx.executeSql("DELETE FROM MyStickys WHERE id = ?",[note.id]);
+		});
+		document.body.removeChild(this.note);
+	},
+	saveSoon: function(){
+		this.cancelPendingSave();
+		var self = this;
+		this._saveTimer = setTimeout(function(){
+			self.save();
+		},200);
+	},
+	cancelPendingSave: function(){
+		if(!("_saveTimer" in this)){
+			return;
+		}
+		clearTimeout(this._saveTimer);
+		delete this._saveTimer;
+	},
+	save: function(){
+		this.cancelPendingSave();
+		if("dirty" in this){
+			this.timestamp = new Date().getTime();
+			delete this.dirty;
+		}
+		
+		var note = this;
+		db.transaction(function(tx){
+			tx.executeSql("Update MyStickys SET note = ?,timestamp = ?, left = ?, top = ?, zindex = ? WHERE id = ?",
+			[note.text, note.timestamp, note.left, note.top, note.zIndex, note.id]);
+		});
+	},
+	saveAsNew: function(){
+		this.timestamp = new Date.getTime();
+		var note = this;
+		db.transaction(function(tx){
+			tx.executeSql("INSERT INTO MyStickys(id, note, timestamp, left, top, zIndex) VALUES(?, ?, ?, ?, ?, ?)",
+			[note.id, note.text, note.timestamp, note.left, note.top, note.zIndex]);
+		});
+	},
+	onMouseDown: function(e){
+		captured = this;
+		this.startX = e.clientX - this.note.offsetLeft;
+        this.startY = e.clientY - this.note.offsetTop;
+        this.zIndex = ++highestZ;
+		
+		var self = this;
+		if (!("mouseMoveHandler" in this)) {
+            this.mouseMoveHandler = function(e) { return self.onMouseMove(e) }
+            this.mouseUpHandler = function(e) { return self.onMouseUp(e) }
+        }
+
+        document.addEventListener('mousemove', this.mouseMoveHandler, true);
+        document.addEventListener('mouseup', this.mouseUpHandler, true);
+
+        return false;
+	},
+	onMouseMove: function(e){
+		if (this != captured)
+		return true;
+		this.left = e.clientX - this.startY + 'px';
+		this.top = e.clientY - this.startY + 'px';
+        return false;
+	}, 
+	onMouseUp: function(e){
+        document.removeEventListener('mousemove', this.mouseMoveHandler, true);
+        document.removeEventListener('mouseup', this.mouseUpHandler, true);
+
+        this.save();
+        return false;
+    },
+    onNoteClick: function(e){
+        this.editField.focus();
+        getSelection().collapseToEnd();
+    },
+    onKeyUp: function(){
+        this.dirty = true;
+        this.saveSoon();
+    },
+
 }
+	
